4aee0bc4fadef99acc0ffbb9ad578c07c308be36
lumigamher <lumigam@hotmail.com>
2024-02-05 19:42:55 -0500

    feat: :heavy_plus_sign: Agregar ventana de visualización de producto en carrito
    Se ha implementado una ventana que permite visualizar el producto a agregar en el carrito. Incluye un input para ingresar la cantidad y un botón para agregar al carrito.

--- js/main.js
+++ js/main.js
@@ -2,3 +2,22 @@ let links = document.querySelector('.header .menu');
 document.querySelector('.botonMenu').addEventListener('click', function(){
     links.classList.toggle('active')
 });
+
+document.addEventListener('DOMContentLoaded', function() {
+    const botonObtener = document.querySelector('.boton');
+    const ventanaAgregar = document.querySelector('.ventana__agregar');
+    const botonAgregar = ventanaAgregar.querySelector('button');
+    const cerrarVentana = ventanaAgregar.querySelector('.cerrar__ventana');
+
+    botonObtener.addEventListener('click', function() {
+        ventanaAgregar.classList.toggle('mostrar');
+    });
+
+    botonAgregar.addEventListener('click', function() {
+        ventanaAgregar.classList.remove('mostrar');
+    });
+
+    cerrarVentana.addEventListener('click', function() {
+        ventanaAgregar.classList.remove('mostrar');
+    });
+});
